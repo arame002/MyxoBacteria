7374feb3d207e3554d4448cc45e88018d7dcee22
arame002 <37461438+arame002@users.noreply.github.com>
2022-07-21 03:08:36 -0700

    Add files via upload

--- build_Bacteria.sh
+++ build_Bacteria.sh
@@ -5,20 +5,21 @@ set -ie
 #if [[ -z $1 ]]; then echo -e "USAGE\n\t$0 BUILD_NAME" && exit 256; fi
 #BUILD_NAME=$1
 
-#module purge
-#module load cmake/3.12.3
+
+#module load cmake
+#module load cuda/9.0
+#module load gcc/6.3.0
 #module load extra
-#module load GCCcore/6.3.0
-
-
+#module load openmpi/2.0.1-slurm-16.05.4
 #rm -rf $BUILD_NAME
 #mkdir -p $BUILD_NAME && cd $BUILD_NAME
 
-cmake ../src/ \
+cmake \
 -DCMAKE_C_COMPILER=$(which gcc)  \
 -DCMAKE_CXX_COMPILER=$(which g++)  \
 -DCUDA_INCLUDE_DIRS=${CUDA_HOME}/include \
 -DBUILD_TESTING=OFF \
+ENABLE_TESTS_COMPILATION=False \
+./
 
-
-make -j 12
+make -j 20
